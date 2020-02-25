99f3ec7e846574f5b27b4f0209ea96e85ce8fb43
VGI <v.girish@castsoftware.com>
2020-02-25 14:28:32 +0530

    IMAGSYS-1567: Add /nginx/html/app-config.js as configurable

---
+++ nginx/html/app-config.js
@@ -0,0 +1,15 @@
+window.config = {
+  basePath: window.defaultBasePath ? `/imaging/${window.defaultBasePath}` : '/imaging',
+  cypherSearch: true,
+  imagingAssistant: false,
+  language: 'English_en', // Allowed values 'English_en' ,'Chinese_ch'
+  isAdmin: true,
+  version: '1.11.0',
+  edgeBrowser: !(false || !!document.documentMode) && !!window.StyleMedia,
+  year: '2020',
+  nodeNumber: 3000, // Limit for progressive rendering - For loading large graph faster
+  edgeNumber: 9000, // Limit for progressive rendering - For loading large graph faster
+  documentationLink: 'https://doc.castsoftware.com/display/IMAGING/CAST+Imaging+System',
+  sessionTimeoutAlertBefore: 20, // In Seconds
+  investigationCount: 6,
+};
