3b656eb42ff5f8a14bb9e703845d43f9b9c6440f
VGI <v.girish@castsoftware.com>
2020-03-09 12:52:15 +0530

    IMAGSYS-0000: Update version to 1.11.3

--- nginx/html/app-config.js
+++ nginx/html/app-config.js
@@ -4,7 +4,7 @@ window.config = {
   imagingAssistant: false,
   language: 'English_en', // Allowed values 'English_en' ,'Chinese_ch'
   isAdmin: true,
-  version: '1.11.0',
+  version: '1.11.3',
   edgeBrowser: !(false || !!document.documentMode) && !!window.StyleMedia,
   year: '2020',
   nodeNumber: 3000, // Limit for progressive rendering - For loading large graph faster
